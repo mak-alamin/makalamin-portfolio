816af43c55766212f93011b1891eae26d651a65a
Mak Alamin <makalamin012@gmail.com>
2023-03-20 13:31:05 +0600

    added sc portfolio

--- src/components/Main.js
+++ src/components/Main.js
@@ -21,10 +21,7 @@ const Main = () => {
         }}
       >
         <div className="overlay">
-          <label
-            htmlFor="my-drawer"
-            className="btn btn-primary drawer-button lg:hidden"
-          >
+          <label htmlFor="my-drawer" className="drawer-button lg:hidden">
             Menu
           </label>
 
@@ -53,14 +50,28 @@ const Main = () => {
           <div className="col-12">
             <p>
               I am a Full Stack Web Developer with a passion for creating
-              beautiful functional websites and web applications. I have a
-              strong background in web development and have worked with PHP,
-              Laravel, Javascript, React, WordPress and more.
+              innovative and dynamic websites and web applications. I have a
+              solid foundation in Javascript, React, PHP, Laravel, WordPress and
+              more.
             </p>
             <br />
             <p>
-              I am currently looking for a position in the web development
-              industry.
+              I am constantly exploring new technologies and trends in the field
+              to stay up-to-date with the latest developments. I am a natural
+              problem solver and have a strong attention to detail, which allows
+              me to create seamless user experiences.
+            </p>
+            <br />
+            <p>
+              As a Full Stack Web Developer, I am committed to delivering
+              high-quality work and exceeding client expectations. I take pride
+              in my ability to work collaboratively with teams and clients to
+              create solutions that meet their unique needs.
+            </p>
+            <br />
+            <p>
+              I'm looking forward to work on your project. Let's make it a great
+              success together.
             </p>
             <a href="#contact" className="button-primary mt-3">
               Contact Me
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,97 +1,144 @@
+import { useEffect, useState } from "react";
+
 const Portfolio = () => {
+  const [portfolioCats, setPortfolioCats] = useState([]);
+  const [portfolios, setPortfolios] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `${process.env.REACT_APP_WP_REST_URL}/elemenfoliocategory?post_type=elemenfolio`
+    )
+      .then((response) => response.json())
+      .then((categories) => {
+        // Output the category names
+        categories.forEach((category) => {
+          // console.log(category.name);
+        });
+
+        setPortfolioCats(categories);
+      })
+      .catch((error) => console.error(error));
+
+    fetch(`${process.env.REACT_APP_WP_REST_URL}/elemenfolio?_embed`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+
+        let pfs = [];
+
+        if (data.length) {
+          data.forEach((el, i) => {
+            let pf = {
+              id: el?.id,
+              title: el?.title?.rendered,
+              content: el?.content?.rendered,
+              featuredImage: el?._embedded["wp:featuredmedia"]["0"].source_url,
+              categories: el?._embedded["wp:term"]["0"],
+            };
+
+            pfs.push(pf);
+          });
+
+          setPortfolios(pfs);
+        }
+      });
+  }, []);
+
+  const [activeTab, setActiveTab] = useState(0);
+  const [showPopup, setShowPopup] = useState(false);
+  const [selectedPortfolio, setSelectedPortfolio] = useState({});
+
+  const handleTabClick = (index) => {
+    setActiveTab(index);
+  };
+
+  const handlePortfolioClick = (portfolio) => {
+    setSelectedPortfolio(portfolio);
+    setShowPopup(true);
+  };
+
+  const closePopup = () => {
+    setShowPopup(false);
+  };
+
+  console.log(portfolioCats);
+
+  console.log(portfolios);
+
   return (
     <section className="portfolio" id="portfolio">
       <h1>My Portfolio</h1>
 
-      <div className="row">
-        <div className="portfolio-item col-4">
-          <a
-            href="https://mak-alamin.github.io/Javascript-Calculator/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="images/simple-javascript-calculator.png"
-              alt="portfolio-1"
-            />
-          </a>
-          <h2>Simple Javascript Calculator</h2>
-          <p>This is simple calculator created with raw javascript coding.</p>
-          <br />
-          <a
-            href="https://github.com/mak-alamin/Javascript-Calculator"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Github Code
-          </a>{" "}
-          <br /> <br />
-          <a
-            href="https://mak-alamin.github.io/Javascript-Calculator/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Live View
-          </a>
+      <div className="portfolio-tabs">
+        <div className="tab-buttons pb-5">
+          {portfolioCats.length &&
+            portfolioCats.map((cat, index) => (
+              <button
+                key={cat.id}
+                onClick={() => handleTabClick(index)}
+                className={activeTab === index ? "active" : ""}
+              >
+                {cat?.name}
+              </button>
+            ))}
         </div>
 
-        <div className="portfolio-item col-4">
-          <a
-            href="https://mak-alamin.github.io/mission-2022"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src="images/mission-2022.png" alt="portfolio-2" />
-          </a>
-          <h2>Mission 2022</h2>
-          <p>
-            This is an assignment project that I created using pure HTML5 and
-            CSS3.
-          </p>
-          <br />
-          <a
-            href="https://github.com/mak-alamin/mission-2022"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Github Code
-          </a>{" "}
-          <br /> <br />
-          <a
-            href="https://mak-alamin.github.io/mission-2022"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Live View
-          </a>
-        </div>
+        {portfolioCats.length &&
+          portfolioCats.map((cat, index) => (
+            <div
+              className={
+                activeTab === index ? "active tab-content" : "tab-content"
+              }
+              key={cat?.id}
+            >
+              {portfolios.length &&
+                portfolios.map((portfolio, i) => (
+                  <div
+                    onClick={() => handlePortfolioClick(portfolio)}
+                    key={portfolio.id}
+                    className="portfolio-item col-3"
+                  >
+                    <img src={portfolio?.featuredImage} alt="portfolio-1" />
+                    <h2>{portfolio?.title}</h2>
+                  </div>
+                ))}
+            </div>
+          ))}
 
-        <div className="portfolio-item col-4">
-          <a
-            href="https://www.thetravelmovement.com.au"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="images/The-Travel-Movement.png"
-              alt="The Travel Movement"
-            />
-          </a>
-          <h2>The Travel Movement</h2>
-          <p>
-            This is a client website from Australlia. I've created their Landing
-            pages using PHP and WordPress.
-          </p>
-          <br />
-          <a
-            href="https://www.thetravelmovement.com.au"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Live View
-          </a>
-        </div>
+        <span class="item break"></span>
       </div>
+
+      {showPopup && (
+        <div className="popup">
+          <div className="popup-content">
+            <button onClick={closePopup} className="btn btn-circle">
+              <svg
+                width="800px"
+                height="800px"
+                viewBox="0 0 24 24"
+                fill="#fff"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M6 6L18 18" stroke="#fff" stroke-linecap="round" />
+                <path
+                  d="M18 6L6.00001 18"
+                  stroke="#fff"
+                  stroke-linecap="round"
+                />
+              </svg>
+            </button>
+
+            <div className="popup-body">
+            <h2>{selectedPortfolio.title}</h2>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: selectedPortfolio.content,
+              }}
+            />
+          </div>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
